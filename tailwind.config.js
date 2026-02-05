60b80e6f092b1c0983fec176b282251a5d717153
ruhul2003 <ruhul941020@gmail.com>
2026-02-05 23:50:54 +0600

    Some section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,14 @@
- 
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{html,js}",
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         oswald: ['Oswald', 'sans-serif'],
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
